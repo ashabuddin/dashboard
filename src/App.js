1a22c6bf40af6c2bb7ac267b73ee5a2c75978e33
Ashab <ashabuddin54321@gmail.com>
2021-09-20 19:40:44 -0700

    das

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
 import {
   BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
 } from "react-router-dom";
 import Home from "./components/Home/Home";
 
 function App() {
   return (
     <Router>
+
+
       <Home/>
     </Router>
   );
